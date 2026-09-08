373ed114265662e66af4c2d453eddb3375744e89
Div709 <soobdiv@gmail.com>
2026-09-08 17:22:38 +0400

    Improved UI and made DC motor work

--- AtlasCockpit/main.py
+++ AtlasCockpit/main.py
@@ -1,6 +1,7 @@
 import sys
 import serial
 import time
+from PyQt6.QtCore import Qt
 
 from PyQt6.QtWidgets import (
     QApplication,
@@ -20,39 +21,58 @@ arduino = serial.Serial("COM5", 9600)
 class Cockpit(QWidget):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("ATLAS COCKPIT")
-        self.resize(400, 250)
+        self.resize(800, 500)
         self.setStyleSheet("""
             QWidget {
                 background-color: #202020;
                 color: lime;
-                font-size: 16px;
+                font-size: 24px;
+                font-weight: bold;
             }
         """)
 
         self.title = QLabel("ATLAS COCKPIT")
         self.throttle = QLabel("Throttle: 0%")
         self.bar = QLabel("[----------]")
-
         self.raw = QLabel("Raw Value: 0")
-        self.rpm = QLabel("RPM: 0")
+        self.rpm = QLabel("0000 RPM")
+        self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.rpm.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.throttle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.bar.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.rpm.setStyleSheet("""
+                font-size: 40px;
+                font-weight: bold;
+                color: cyan;
+                """)
+        self.throttle.setStyleSheet("""
+                font-size: 22px;#
+                """)
         self.warning = QLabel("STATUS: NORMAL")
         self.status = QLabel("STATUS: READY")
-        self.engine_state = "OFF"
-        self.start_time = None
-        self.last_button_state = 1
+        self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.power_led = QLabel("● POWER")
+        self.engine_led = QLabel("● ENGINE")
+        self.warning_led = QLabel("● WARNING")
+        self.power_led.setStyleSheet("color: green; font-size: 22px;")
+
 
 
         layout = QVBoxLayout()
-        layout.addWidget(self.bar)
-
         layout.addWidget(self.title)
         layout.addWidget(self.throttle)
+        layout.addWidget(self.bar)
         layout.addWidget(self.raw)
         layout.addWidget(self.status)
         layout.addWidget(self.warning)
         layout.addWidget(self.rpm)
+        layout.addWidget(self.power_led)
+        layout.addWidget(self.engine_led)
+        layout.addWidget(self.warning_led)
 
         self.setLayout(layout)
 
@@ -62,67 +82,66 @@ class Cockpit(QWidget):
 
     def update_data(self):
 
-        line = arduino.readline().decode().strip()
-        print(line)
+        try:
+            line = arduino.readline().decode().strip()
+            print(line)
 
-        parts = line.split(",")
+            parts = line.split(",")
 
-        pot_value = int(parts[0].split("=")[1])
-        engine_state = int(parts[1].split("=")[1])
+            pot_value = int(parts[0].split("=")[1])
+            engine_state = parts[1].split("=")[1]
+            rpm_from_arduino = int(parts[2].split("=")[1])
+        except Exception as e:
+            print(e)
+            return
 
         percent = int((pot_value / 1023) * 100)
         # Start engine
 
         # Detect button press edge
 
-        if (
-                engine_state == 0
-                and self.last_button_state == 1
-                and self.engine_state == "OFF"
-        ):
-            self.engine_state = "STARTING"
-            self.start_time = time.time()
-
-        self.last_button_state = engine_state
-
-
-        # Engine starting
-
-        if self.engine_state == "STARTING":
-
-            self.status.setText("ENGINE: STARTING...")
-
-            if time.time() - self.start_time > 2:
-                self.engine_state = "RUNNING"
-
-        # Engine running
-
-        elif self.engine_state == "RUNNING":
-
-            self.status.setText("ENGINE: RUNNING")
-
-        # Engine off
-
-        elif self.engine_state == "OFF":
-
+        if engine_state == "OFF":
             self.status.setText("ENGINE: OFF")
 
-        print(self.engine_state)
-        if self.engine_state == "RUNNING":
-            rpm = int((percent / 100) * 5000)
+        elif engine_state == "STARTING":
+            self.status.setText("ENGINE: STARTING...")
+
+        elif engine_state == "RUNNING":
+            self.status.setText("ENGINE: RUNNING")
+
+        if engine_state == "RUNNING":
+            self.engine_led.setStyleSheet(
+                "color: green; font-size:22px;"
+            )
         else:
-            rpm = 0
+            self.engine_led.setStyleSheet(
+                "color: gray; font-size:22px;"
+            )
+
+
+
+        rpm = rpm_from_arduino
+
         if rpm < 500:
             self.warning.setText("⚠ ENGINE IDLE")
             self.warning.setStyleSheet("color: yellow;")
+            self.warning_led.setStyleSheet(
+                "color: yellow; font-size:22px;"
+            )
 
         elif rpm > 4500:
             self.warning.setText("⚠ HIGH RPM WARNING")
             self.warning.setStyleSheet("color: red;")
+            self.warning_led.setStyleSheet(
+                "color: red; font-size:22px;"
+            )
 
         else:
             self.warning.setText("✅ SYSTEM NORMAL")
             self.warning.setStyleSheet("color: lime;")
+            self.warning_led.setStyleSheet(
+                "color: gray; font-size:22px;"
+            )
 
         bars = int(percent / 10)
 
@@ -131,7 +150,7 @@ class Cockpit(QWidget):
         self.throttle.setText(f"Throttle: {percent}%")
         self.bar.setText(gauge)
         self.raw.setText(f"Raw Value: {pot_value}")
-        self.rpm.setText(f"RPM: {rpm}")
+        self.rpm.setText(f"{rpm:04d}RPM")
 
 
 
